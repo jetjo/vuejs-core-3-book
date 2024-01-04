a504c457832c78bb5f54c1ac15130d9a2938c624
jetjo <bingsea@foxmail.com>
2024-01-04 17:16:58 +0800

    delete test file

--- 二、响应系统/5、非原始值的响应式方案/7、代理数组/lengthSet.js
+++
@@ -1,9 +0,0 @@
-import { effect } from "./effect/index.js";
-import { reactive } from "./reactive/index.js";
-const state = reactive([]);
-
-effect(() => {
-  console.log(state.length);
-});
-
-state.length = 100;
--- 二、响应系统/5、非原始值的响应式方案/7、代理数组/reactive/traps/array/convention.js
+++
@@ -1,3 +0,0 @@
-const ACT_AS_ARRAY_FLAG = Symbol('act_as_array_like_flag')
-
-export { ACT_AS_ARRAY_FLAG }
