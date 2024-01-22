601bdbb2d3ba20766c8b04896c3907a5f9ad4fef
jetjo <bingsea@foxmail.com>
2024-01-22 14:25:29 +0800

    path alias

---
+++ src/reactive/_traps/Reactive/5-6.js
@@ -0,0 +1,3 @@
+import getReactive from '../../../5、非原始值的响应式方案/6、浅只读与深只读/reactive/traps/Reactive.js'
+
+export default getReactive
---
+++ src/reactive/_traps/Reactive/5-8.js
@@ -0,0 +1,3 @@
+import getReactive from '../../../5、非原始值的响应式方案/8、代理Set和Map/reactive/traps/Reactive.js'
+
+export default getReactive
