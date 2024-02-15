c058bb5a8425645dbdd7500fab32687ae75c4ffc
jetjo <bingsea@foxmail.com>
2024-02-15 09:58:03 +0800

    fix error

--- 三、渲染器/types/shims.d.ts
+++ 三、渲染器/types/shims.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="./api-g.d.ts" />
-/// <reference types="./Event-g.d.ts" />
+/// <reference types="./event-dom-g.d.ts" />
 
 export type * from '../7、渲染器的设计/3-自定义渲染器/api-c.d.ts'
 export type * from '../8、挂载与更新/3-正确地设置元素属性/api-f.d.ts'
