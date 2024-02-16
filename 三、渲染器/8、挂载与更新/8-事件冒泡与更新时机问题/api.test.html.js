ac4e889c2a17925a737f714df334bf3c15c78288
jetjo <bingsea@foxmail.com>
2024-02-16 11:50:17 +0800

    8-8 事件冒泡与更新时机问题

---
+++ 三、渲染器/8、挂载与更新/8-事件冒泡与更新时机问题/api.test.html.js
@@ -0,0 +1,63 @@
+import { ref, effect } from '@vue/reactivity'
+import creatorFactory from '../6-区分vnode类型/api.js'
+// import optionCreator from '../7-事件的处理/render-opt-browser.js'
+// import optionCreator from './render-opt-browser.js'
+import { getApi, isLatestVer, fixRenderForTest3_8_7 } from '../../utils/test.helper.js'
+import { warn } from '#root/utils'
+
+// @ts-ignore
+export async function test(optionCreator, title = '8-事件冒泡与更新时机问题', isBrowser = false) {
+  if (await isLatestVer(optionCreator, creatorFactory, isBrowser)) {
+    const parentHasProps = ref(false)
+    // @ts-ignore
+    function getVnode() {
+      const eventKey = 'onClick'
+      const eventName = eventKey.slice(2).toLowerCase()
+      const parentNodeType = 'div'
+      const nodeType = 'p'
+      // NOTE: 不能包含大写字母,会被转换为小写
+      // const attrForTest = 'test-name' // 'testName'
+      /**@type {*} */
+      const vnode = {
+        type: parentNodeType,
+        props: parentHasProps.value
+          ? {
+              [eventKey]: (/** @type {Event} */ e) => {
+                alert(`绑定了${eventName}事件`)
+              }
+            }
+          : {},
+        children: [
+          {
+            type: 'span',
+            children: parentHasProps.value ? 'click me' : ''
+          },
+          {
+            type: nodeType,
+            props: {
+              [eventKey]: () => {
+                parentHasProps.value = !parentHasProps.value
+              }
+            },
+            children: 'click me'
+          }
+        ]
+      }
+
+      warn({ vnode })
+      return vnode
+    }
+
+    const { render, container, config } = await getApi(
+      optionCreator,
+      creatorFactory,
+      title,
+      `没有调用事件发生后才绑定的'handler'`
+    )
+    fixRenderForTest3_8_7(render, config) //, `8-事件冒泡与更新时机问题-html`)
+    effect(() => {
+      render(getVnode(), container) //, `8-事件冒泡与更新时机问题-html`)
+      // render(getVnode(), container, parentHasProps.value ? 'effect re-run' : undefined)
+    })
+  }
+}
